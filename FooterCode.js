23c15df398e3acc141e6d65767a1be786c2cb7c2
MontyBCHS <98117621+MontyBCHS@users.noreply.github.com>
2025-09-06 22:16:38 -0600

    Add files via upload

---
+++ FooterCode.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+let myFooterCode = ' <hr><p><a href="../index.html">Return to Profile Card</a></p><center><img src="../Images/MrMontgomery-YellowWritingBlueOutline.png" width="715" height="150" alt=""/> <img src="../Images/SciLogo-Ver3.gif" width="150px" height="150px" alt=""/></center> ';
+document.getElementById("myFooter").innerHTML = myFooterCode;
